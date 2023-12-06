dc26699b42a28da8a9228d2a086f4032f150884a
VyVictory <vyvictory2003@gmail.com>
2023-12-06 15:34:04 +0700

    update sữa lỗi cấp tài khoản

--- front-end/src/webfile/views/qlhoso/quanlycongtacnhanvien/Captaikhoan.jsx
+++ front-end/src/webfile/views/qlhoso/quanlycongtacnhanvien/Captaikhoan.jsx
@@ -5,7 +5,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import bcrypt from 'bcryptjs';
 import '../../../viewcss/qlhoso/qlthongtinnhanvien/Captaikhoan.css'
 const Captaikhoan = () => {
-    const [listacc, setListacc] = useState([])
     const navigate = useNavigate()
     const [dsacc, setDsacc] = useState([])
     //const UQuyenhang = sessionStorage.getItem('UQuyenhang');
@@ -51,110 +50,109 @@ const Captaikhoan = () => {
     };
 
     const [isVisible, setIsVisible] = useState(false);
+    const [isVisibleds, setIsVisibleds] = useState(true);
 
     const toggleVisibility = () => {
         setIsVisible(!isVisible);
+        setIsVisibleds(!isVisibleds)
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try {
-            const hashedPassword = await hashPassword(account.Password);
-            const dataToSend = {
-                User: account.User,
-                Email: account.Email,
-                Password: hashedPassword,
-                Quyenhang: account.Quyenhang,
-                Quyentruyvan: selectedCheckboxes
-            };
 
-            // Kiểm tra mật khẩu và mật khẩu xác nhận
-            if (account.Quyenhang < 1) {
-                alert('quyền hạng không nhỏ hơn 1');
-                return;
-            }
-            else if (account.Password !== account.ConfirmPassword) {
-                alert('Mật khẩu xác nhận không khớp.');
-                return;
-            }
-            else if (dataToSend.Quyentruyvan === '') {
-                alert('Chọn ít nhất một quyền truy vấn');
-                return;
-            }
-            else {
-                let trunguser = '';
-                listacc.map(e => {
-                    if (e.User === dataToSend.User) {
-                        trunguser = 'co';
-                    }
-                    else { trunguser = 'khong' }
-                })
-                if (trunguser === 'co') {
-                    alert('User ' + dataToSend.User + ' đã được đăng ký');
-                }
-                else if (trunguser === 'khong') {
-                    axios.post('http://localhost:3000/account', dataToSend)
-                        .then(_res => {
-                            alert('cấp thành công tài khoản có user:' + dataToSend.User);
-                            window.location.reload();
-                        }).catch(err => console.log(err));
-                }
-            }
-        } catch (error) {
-            console.error(error);
+        const hashedPassword = await hashPassword(account.Password);
+        const dataToSend = {
+            User: account.User,
+            Email: account.Email,
+            Password: hashedPassword,
+            Quyenhang: account.Quyenhang,
+            Quyentruyvan: selectedCheckboxes
+        };
+
+        // Kiểm tra mật khẩu và mật khẩu xác nhận
+        if (account.Quyenhang < 1) {
+            alert('quyền hạng không nhỏ hơn 1');
+            return;
         }
+        else if (account.Password !== account.ConfirmPassword) {
+            alert('Mật khẩu xác nhận không khớp.');
+            return;
+        }
+        else if (dataToSend.Quyentruyvan === '') {
+            alert('Chọn ít nhất một quyền truy vấn');
+            return;
+        }
+        else {
+            const existingUser = dsacc.find(acc => String(acc.User) === String(dataToSend.User));
+
+            if (existingUser) {
+                alert('User tồn tại');
+            } else {
+                axios.post('http://localhost:3000/account', dataToSend)
+                    .then(_res => {
+                        alert(existingUser + 'cấp thành công tài khoản có user: ' + dataToSend.User);
+                        window.location.reload();
+                    })
+                    .catch(err => console.error(err));
+            }
+        }
+
+
+    };
+    function deleteaccount(a) {
+        axios.delete("http://localhost:3000/account/" + a)
+            .then(res => {
+                if (res.data) { setDsacc(dsacc.filter(acc => acc._id !== a)); } else (alert(res.data)
+                )
+            })
+        return null;
     };
 
-    return (
+    return (<div>
         <div className='ctk-all'>
-            <div className='ctk-list-account'>
-                <div >
-                    <button onClick={toggleVisibility}>Cấp Tài Khoản</button>
+            {isVisibleds && (
+                <div className='ctk-list-account'>
+                    <div >
+                        <button onClick={toggleVisibility}>Cấp Tài Khoản</button>
+                    </div>
+                    <div className="">
+                        <h3>Danh Sách Tài Khoản</h3>
+                    </div>
+                    <div className="">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>UserName</th>
+                                    <th>Email</th>
+                                    <th>Quyền Hạng</th>
+                                    <th>Quyền Truy Vấn</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    dsacc.slice()
+                                        .sort((a, b) => a.Quyenhang - b.Quyenhang)
+                                        .map(ac => (
+                                            <tr>
+                                                <td>{ac.User}</td>
+                                                <td>{ac.Email}</td>
+                                                <td>{ac.Quyenhang}</td>
+                                                <td>{ac.Quyentruyvan.replace(/^,/, '')}</td>
+                                                <td className="">
+                                                    <button onClick={toggleVisibility} className='btn btn-info btn-sm me-2'>Chỉnh sữa</button>
+                                                    <button onClick={() => deleteaccount(ac._id)} className='btn btn-warning btn-sm'>Xoa</button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div className="">
-                    <h3>Danh Sách Tài Khoản</h3>
-                </div>
-                <div className="">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>UserName</th>
-                                <th>Email</th>
-                                <th>Quyền Hạng</th>
-                                <th>Quyền Truy Vấn</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                dsacc.slice()
-                                    .sort((a, b) => a.Quyenhang - b.Quyenhang)
-                                    .map(ac => (
-                                        <tr key={ac._id}>
-                                            {/* Các cột thông tin của tài khoản */}
-                                            <td>{ac.User}</td>
-                                            <td>{ac.Email}</td>
-                                            <td>{ac.Quyenhang}</td>
-                                            <td>{ac.Quyentruyvan}</td>
-                                            <td className="">
-                                                <Link to={'/' + ac._id} className='btn btn-info btn-sm me-2'>
-                                                    Chinh sua
-                                                </Link>
-                                                <button className='btn btn-warning btn-sm'>
-                                                    Xoa
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    ))
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
+            )}
             <div className='ctk-box'>
                 {isVisible && (
-
                     <div className='ctk-container'>
                         <div className='ctk-div-bt-tac'>
                             <button onClick={toggleVisibility} className='ctk-bt-tac'>X</button>
@@ -164,61 +162,65 @@ const Captaikhoan = () => {
                                 <h2>Cấp Tài Khoản</h2>
                             </div>
 
-                            <form className='ctk-form' id="myForm" onSubmit={handleSubmit}>
-                                <div className='ctk-input-conten'>
-                                    <div className='ctk-input-text'>
-                                        <label className='ctk-tille-input'>UserName:</label><br />
-                                        <input className='ctk-input' required type="text" onChange={(e) => setAccount({ ...account, User: e.target.value })} placeholder='Nhập tên tài khoản' /><br />
-                                        <label className='ctk-tille-input'>Password:</label><br />
-                                        <input className='ctk-input' required type="password" onChange={(e) => setAccount({ ...account, Password: e.target.value })} placeholder='Nhập mật khẩu' /><br />
-                                        <label className='ctk-tille-input'>Confirm Password:</label><br />
-                                        <input className='ctk-input' required type="password" onChange={(e) => setAccount({ ...account, ConfirmPassword: e.target.value })} placeholder='Xác nhận mật khẩu' /><br />
-                                        <label className='ctk-tille-input'>Email:</label><br />
-                                        <input className='ctk-input' required type="email" onChange={(e) => setAccount({ ...account, Email: e.target.value })} placeholder='Nhập email' /><br />
-                                        <label className='ctk-tille-input'>Quyền Hạng Chức Vụ:</label>
-                                        <span class="ctk-tooltip-container">
-                                            <span class="ctk-hover-element" title="Quyền Hạng này phân cấp cho chức vụ nhằm xác định chức vụ lớn hơn cao nhất là 1"> ?</span>
-                                        </span><br />
-                                        <input className='ctk-input' required type='number' onChange={(e) => setAccount({ ...account, Quyenhang: e.target.value })} placeholder='Nhập Quyền Hạng' />
+                            <div className='ctk-form'></div>
 
-                                        <br />
-                                    </div>
-                                    <div className='ctk_quyentruyvan'>
-                                        <label>Quyền Truy Vấn:</label><br />
+                            <div className='ctk-input-conten'>
+                                <div className='ctk-input-text'>
+                                    <label className='ctk-tille-input'>UserName:</label><br />
+                                    <input className='ctk-input' required type="text" onChange={(e) => setAccount({ ...account, User: e.target.value })} placeholder='Nhập tên tài khoản' /><br />
+                                    <label className='ctk-tille-input'>Password:</label><br />
+                                    <input className='ctk-input' required type="password" onChange={(e) => setAccount({ ...account, Password: e.target.value })} placeholder='Nhập mật khẩu' /><br />
+                                    <label className='ctk-tille-input'>Confirm Password:</label><br />
+                                    <input className='ctk-input' required type="password" onChange={(e) => setAccount({ ...account, ConfirmPassword: e.target.value })} placeholder='Xác nhận mật khẩu' /><br />
+                                    <label className='ctk-tille-input'>Email:</label><br />
+                                    <input className='ctk-input' required type="email" onChange={(e) => setAccount({ ...account, Email: e.target.value })} placeholder='Nhập email' /><br />
+                                    <label className='ctk-tille-input'>Quyền Hạng Chức Vụ:</label>
+                                    <span class="ctk-tooltip-container">
+                                        <span class="ctk-hover-element" title="Quyền Hạng này phân cấp cho chức vụ nhằm xác định chức vụ lớn hơn cao nhất là 1"> ?</span>
+                                    </span><br />
+                                    <input className='ctk-input' required type='number' onChange={(e) => setAccount({ ...account, Quyenhang: e.target.value })} placeholder='Nhập Quyền Hạng' />
 
-                                        <input className='ctk-checkbox' type="checkbox" name="them" onChange={handleCheckboxChange}></input>
-                                        <label htmlFor="v1">Thêm</label><br />
-
-                                        <input className='ctk-checkbox' type="checkbox" name="sua" onChange={handleCheckboxChange}></input>
-                                        <label htmlFor="v2">Sửa</label><br></br>
-
-                                        <input className='ctk-checkbox' type="checkbox" name="xoa" onChange={handleCheckboxChange} ></input>
-                                        <label htmlFor="v3">Xóa</label><br></br>
-
-                                        <input className='ctk-checkbox' type="checkbox" name="chamcong" onChange={handleCheckboxChange}></input>
-                                        <label htmlFor="v4">Chấm Công</label><br></br>
-
-                                        <input className='ctk-checkbox' type="checkbox" name="qlcongtac" onChange={handleCheckboxChange}></input>
-                                        <label htmlFor="v5">Quản Lý Công Tác Nhân Viên</label><br></br>
-
-                                        <input className='ctk-checkbox' type="checkbox" name="qlcalam" onChange={handleCheckboxChange}></input>
-                                        <label htmlFor="v6">Quản Lý Ca</label><br></br>
-
-                                        <input className='ctk-checkbox' type="checkbox" name="qldanhsach" onChange={handleCheckboxChange}></input>
-                                        <label htmlFor="v7">Quản Lý Danh Sách Nhân Sự</label><br></br>
-                                    </div>
-
+                                    <br />
                                 </div>
+                                <div className='ctk_quyentruyvan'>
+                                    <label>Quyền Truy Vấn:</label><br />
+
+                                    <input className='ctk-checkbox' type="checkbox" name="them" onChange={handleCheckboxChange}></input>
+                                    <label htmlFor="v1">Thêm</label><br />
+
+                                    <input className='ctk-checkbox' type="checkbox" name="sua" onChange={handleCheckboxChange}></input>
+                                    <label htmlFor="v2">Sửa</label><br></br>
+
+                                    <input className='ctk-checkbox' type="checkbox" name="xoa" onChange={handleCheckboxChange} ></input>
+                                    <label htmlFor="v3">Xóa</label><br></br>
+
+                                    <input className='ctk-checkbox' type="checkbox" name="chamcong" onChange={handleCheckboxChange}></input>
+                                    <label htmlFor="v4">Chấm Công</label><br></br>
+
+                                    <input className='ctk-checkbox' type="checkbox" name="qlcongtac" onChange={handleCheckboxChange}></input>
+                                    <label htmlFor="v5">Quản Lý Công Tác Nhân Viên</label><br></br>
+
+                                    <input className='ctk-checkbox' type="checkbox" name="qlcalam" onChange={handleCheckboxChange}></input>
+                                    <label htmlFor="v6">Quản Lý Ca</label><br></br>
+
+                                    <input className='ctk-checkbox' type="checkbox" name="qldanhsach" onChange={handleCheckboxChange}></input>
+                                    <label htmlFor="v7">Quản Lý Danh Sách Nhân Sự</label><br></br>
+                                </div>
+
+                            </div>
+                            <form id="myForm" onSubmit={handleSubmit}>
                                 <div className='ctk-bt-tao'>
                                     <input type='submit' className='ctk-nut' value="Tạo Tài Khoản" />
                                 </div>
-
                             </form>
+
                         </div>
                     </div>
 
                 )}</div>
         </div>
+    </div>
+
     )
 
 }
